1c80f067d51cd8d0ed9faa074a73e5596b0c3cc9
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-13 00:11:51 +0100

    Build improved

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-export TAG="webapp:latest"
+export IMAGE="webapp"
+export TAG="latest"
+export REGISTRY="registryuoc:5050"
 
-docker build -t $TAG ./
-echo "Built: $TAG"
+docker build -t ${IMAGE}:${TAG} ./
+echo "Image Built: $IMAGE Tag: $TAG"
+docker tag ${IMAGE}:${TAG} ${REGISTRY}/${IMAGE}:${TAG}
+docker push ${REGISTRY}/${IMAGE}:${TAG}
+docker rmi ${IMAGE}:${TAG}
+docker rmi python:3.6.0-alpine
