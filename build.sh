8b810930b3721a314ea29293b1940dbad459a3ee
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-13 00:19:14 +0100

    Info stand out

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ export TAG="latest"
 export REGISTRY="registryuoc:5050"
 
 docker build -t ${IMAGE}:${TAG} ./
+echo "============================================================="
 echo "Image Built: $IMAGE Tag: $TAG"
+echo "============================================================="
 docker tag ${IMAGE}:${TAG} ${REGISTRY}/${IMAGE}:${TAG}
 docker push ${REGISTRY}/${IMAGE}:${TAG}
+echo "============================================================="
+echo "Image $IMAGE pushed to $REGISTRY with tag $TAG"
+echo "============================================================="
 docker rmi ${IMAGE}:${TAG}
 docker rmi python:3.6.0-alpine
