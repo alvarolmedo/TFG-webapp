cdf914c750239686eef62c0acb965458b745e0a3
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-13 00:22:58 +0100

    [FIX]Deploy from jenkins localhost is the container, not host

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 #starting
 docker-compose up -d
 
-curl -XPUT -D- 'http://localhost:9200/.kibana/doc/index-pattern:tfg-uoc' \
+curl -XPUT -D- 'http://172.16.0.1:9200/.kibana/doc/index-pattern:tfg-uoc' \
     -H 'Content-Type: application/json' \
     -d '{"type": "index-pattern", "index-pattern": {"title": "logstash-*", "timeFieldName": "@timestamp"}}'
 
-cd kibana-data
-cd searches
-curl -H "Content-Type: application/json" -v -XPOST 'http://localhost:9200/tfg-uoc/search/all' -d @all.json
-cd ../visualizations
+# cd kibana-data
+# cd searches
+# curl -H "Content-Type: application/json" -v -XPOST 'http://172.16.0.1:9200/tfg-uoc/search/all' -d @all.json
+# cd ../visualizations
