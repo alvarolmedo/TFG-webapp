074ef8265679d191a44f18edd00d7669d4792c52
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-14 01:24:30 +0100

    Importing kibana

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,17 @@
 #starting
 docker-compose up -d
 
+echo "==========================="
+echo "== Putting index-pattern =="
+echo "==========================="
+
 curl -XPUT -D- 'http://172.16.0.1:9200/.kibana/doc/index-pattern:tfg-uoc' \
     -H 'Content-Type: application/json' \
     -d '{"type": "index-pattern", "index-pattern": {"title": "logstash-*", "timeFieldName": "@timestamp"}}'
 
-# cd kibana-data
-# cd searches
-# curl -H "Content-Type: application/json" -v -XPOST 'http://172.16.0.1:9200/tfg-uoc/search/all' -d @all.json
-# cd ../visualizations
+echo "=============================="
+echo "== Importing kibana objects =="
+echo "=============================="
+
+cd kibana-data
+./kibana-importer.py --json everything.json --kibana-url http://172.16.0.1:5601
