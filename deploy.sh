b8a04e18569d550a4fca8b32bdb1f5f9519e2592
root <root@localhost.localdomain>
2019-04-01 16:49:27 +0200

    No ELK references

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,3 @@ echo "======================"
 
 docker-compose up -d
 
-echo "==========================="
-echo "== Putting index-pattern =="
-echo "==========================="
-
-curl -XPUT 'http://172.16.0.1:9200/.kibana/doc/index-pattern:tfg-uoc' \
-    -H 'Content-Type: application/json' \
-    -d '{"type": "index-pattern", "index-pattern": {"title": "logstash-*", "timeFieldName": "@timestamp"}}'
-
-echo ""
-echo "=============================="
-echo "== Importing kibana objects =="
-echo "=============================="
-
-cd kibana-objects
-./kibana-importer.py --json everything.json --kibana-url http://172.16.0.1:5601
